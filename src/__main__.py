b73031952b0cdea2d5d52bc03e8da08bad3a9706
rabbithy <thiago.coelho158@gmail.com>
2020-10-19 13:23:50 -0300

    r

---
+++ src/__main__.py
@@ -0,0 +1,38 @@
+import pyglet
+
+from app.build import build_main_frame
+
+display = pyglet.canvas.Display()
+screen = display.get_default_screen()
+
+WIDTH = screen.width
+HEIGHT = screen.height - 25
+FPS = 200
+DELTA = 1/FPS
+
+
+def main():
+    window = pyglet.window.Window(WIDTH, HEIGHT, "Fyks", resizable=True, vsync=False)
+    pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
+    pyglet.gl.glBlendFunc(pyglet.gl.GL_SRC_ALPHA, pyglet.gl.GL_ONE_MINUS_SRC_ALPHA)
+
+    frame = build_main_frame(WIDTH, HEIGHT)
+    window.push_handlers(frame)
+
+
+    @window.event
+    def on_draw():
+        window.clear()
+        frame.draw()
+
+
+    def update(dt):
+        frame.update(DELTA)
+
+
+    pyglet.clock.schedule_interval(update, DELTA)
+    pyglet.app.run()
+
+
+if __name__ == '__main__':
+    main()
