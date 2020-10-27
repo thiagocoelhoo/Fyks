8c756efea20de8b27d3d50631fde67815dce4e6a
rabbithy <thiago.coelho158@gmail.com>
2020-10-27 12:18:50 -0300

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -12,28 +12,13 @@ FPS = 120
 DELTA = 1/FPS
 
 
-def main():
-    try:
-        config = pyglet.gl.Config(
-            sample_buffers=1, 
-            samples=4,
-            depth_size=16,
-            double_buffer=True)
-        window = pyglet.window.Window(
-            width=WIDTH, 
-            height=HEIGHT, 
-            caption="Fyks", 
-            config=config,
-            resizable=True, 
-            vsync=False)
-    except pyglet.window.NoSuchConfigException:
-        print('Error: noSuchConfigException')
-        window = pyglet.window.Window(
-            width=WIDTH, 
-            height=HEIGHT, 
-            caption="Fyks",
-            resizable=True, 
-            vsync=False)
+def main():    
+    window = pyglet.window.Window(
+        width=WIDTH, 
+        height=HEIGHT, 
+        caption="Fyks",
+        resizable=True, 
+        vsync=False)
     
     pyglet.gl.glEnable(pyglet.gl.GL_BLEND)
     pyglet.gl.glBlendFunc(pyglet.gl.GL_SRC_ALPHA, pyglet.gl.GL_ONE_MINUS_SRC_ALPHA)
