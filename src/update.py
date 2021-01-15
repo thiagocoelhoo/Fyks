1fd549310498d59bfecb3b03d1bd615fb45bea71
rabbithy <thiago.coelho158@gmail.com>
2021-01-15 13:07:00 -0300

    Create check_updates function

--- src/update.py
+++ src/update.py
@@ -3,10 +3,19 @@ import re
 import json
 from zipfile import ZipFile
 
+releases_url = 'https://api.github.com/repos/Rabbithy/Fyks/releases'
+
+
+def check_updates(current_version):
+    response = urllib.request.urlopen(releases_url)
+    data =  json.loads(response.read().decode('utf-8'))
+    release = data[0]
+    version = re.search('(\d+\.?)+', release['tag_name'])
+    return version > current_version
+
 
 def update(current_version):
-    url = 'https://api.github.com/repos/Rabbithy/Fyks/releases'
-    response = urllib.request.urlopen(url)
+    response = urllib.request.urlopen(releases_url)
     data =  json.loads(response.read().decode('utf-8'))
     release = data[0]
     version = re.search('(\d+\.?)+', release['tag_name'])
