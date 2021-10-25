6ff19accf7f5bc22ec24d8a76eb78b59f63986f9
rabbithy <thiago.coelho158@gmail.com>
2021-10-25 14:35:44 -0300

    Update singleton.py

--- src/utils/singleton.py
+++ src/utils/singleton.py
@@ -5,3 +5,7 @@ class Singleton(type):
         if cls._instances.get(cls) is None:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
+    
+    def get(cls):
+        print(cls, cls._instances)
+        return cls._instances.get(cls)
